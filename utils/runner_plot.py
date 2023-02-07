6ba9b0220300250d80714292cf71b6c30bbc5c28
Manu <manuel.munoz.sanchez@gmail.com>
2023-02-07 13:44:49 +0100

    Some updates to easily run runner_plot.py on all files

--- utils/runner_plot.py
+++ utils/runner_plot.py
@@ -5,7 +5,7 @@ import argparse
 from plotting_scenarios import plot_all_scenarios
 from logger.logger import *
 from pathlib import Path
-
+import glob
 
 # working directory
 ROOT = Path(__file__).parent.parent
@@ -53,23 +53,56 @@ DATADIR = args.data
 FIGDIR = args.fig
 RESULTDIR = args.result
 
+
+def get_all_file_numbers(args):
+    file_numbers = []
+    file_paths = glob.glob(f"{args.result}*_solo.json")
+    for file in file_paths:
+        file_number = file.split("_")[1]
+        file_numbers.append(file_number)
+
+    # Should not happen, but just make sure we don't have duplicates
+    numbers_set = set(file_numbers)
+    file_numbers = list(sorted(numbers_set))
+
+    return file_numbers
+
+
 if __name__ == "__main__":
-    start = time.perf_counter()
-    FILENUM = args.filenum
-    RESULT_TIME = f"2022-{args.result_time}"
-    FILE = f"training_tfexample.tfrecord-{FILENUM}-of-01000"
-    RESULT_FILENAME = f'Waymo_{FILENUM}_{RESULT_TIME}_tag.json'
-    RESULT_SOLO = f'Waymo_{FILENUM}_{RESULT_TIME}_solo.json'
-    # sanity check
-    FILENAME = os.path.join(DATADIR, FILE)
-    if os.path.exists(FILENAME):
-        print(f"Plotting:{FILE}")
-        try:
-            _ = plot_all_scenarios(DATADIR, FILE, FILENUM, RESULTDIR, RESULT_FILENAME, RESULT_SOLO, FIGDIR)
-        except Exception as e:
-            logger.error(f"FILE: {FILENUM}.{e}")
+
+    RESULT_TIME = f"{args.result_time}"
+
+    if args.filenum == "all":
+        file_numbers = get_all_file_numbers(args)
+        print("Filenumbers", file_numbers)
     else:
-        print(f"File not found:{FILE}")
-        logger.info(f"File not found:{FILENAME}")
-    end = time.perf_counter()
-    logger.info(f"DATA:{FILENUM}.JSON:{RESULT_SOLO}.Run time: {end - start}")
+        file_numbers = [args.file_number]
+
+    for file_number in file_numbers:
+        start = time.perf_counter()
+        if "train" in args.data:
+            FILE = f"training_tfexample.tfrecord-{file_number}-of-01000"
+        else:
+            FILE = f"validation_tfexample.tfrecord-{file_number}-of-00150"
+
+        RESULT_FILENAME = f'Waymo_{file_number}_{RESULT_TIME}_tag.json'
+        RESULT_SOLO = f'Waymo_{file_number}_{RESULT_TIME}_solo.json'
+        # sanity check
+        FILENAME = os.path.join(DATADIR, FILE)
+        print("attempting ", FILENAME)
+        if os.path.exists(FILENAME):
+            print(f"Plotting:{FILE}")
+            try:
+                _ = plot_all_scenarios(DATADIR, FILE, file_number, RESULTDIR, RESULT_FILENAME, RESULT_SOLO, FIGDIR)
+
+            except Exception as e:
+                print(e)
+                raise e
+                logger.error(f"FILE: {file_number}.{e}")
+        else:
+            print(f"File not found:{FILE}")
+            print(f"Filename not found:{FILENAME}")
+
+            logger.info(f"File not found:{FILENAME}")
+        end = time.perf_counter()
+        logger.info(f"DATA:{file_number}.JSON:{RESULT_SOLO}.Run time: {end - start}")
